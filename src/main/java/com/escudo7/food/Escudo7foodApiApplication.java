c7ff0d26ba24cb49b8260cd4e0dcc63780c05b05
Riosney Santos <riosney_s@hotmail.com>
2021-08-28 15:07:11 -0300

    Subindo projto

---
+++ src/main/java/com/escudo7/food/Escudo7foodApiApplication.java
@@ -0,0 +1,13 @@
+package com.escudo7.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Escudo7foodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Escudo7foodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escudo7/food/Escudo7foodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escudo7.food;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Escudo7foodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
