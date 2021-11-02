7ee16be5dc0f835a174cc703d8eb0326b2c549da
Riosney Santos <riosney.vale@saude.ce.gov.br>
2021-11-02 16:26:03 -0300

    Criando adicionar e ResponseStatus

--- src/main/java/com/escudo7/food/api/controller/CozinhaController.java
+++ src/main/java/com/escudo7/food/api/controller/CozinhaController.java
@@ -1,5 +1,6 @@
 package com.escudo7.food.api.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
@@ -8,7 +9,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.escudo7.food.api.model.CozinhasXmlWrapper;
@@ -40,9 +44,15 @@ public class CozinhaController {
 			return ResponseEntity.ok(cozinha);
 		}
 		
-//		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-		
 		return ResponseEntity.notFound().build();
 	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+	
+	
 
 }
