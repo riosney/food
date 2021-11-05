7e910fa566a1b2eaa91d28d1d542a4189d6ec00c
Riosney Santos <riosney.vale@saude.ce.gov.br>
2021-11-05 14:35:06 -0300

    Criando atualizar restaurantes

--- src/main/java/com/escudo7/food/api/controller/RestauranteController.java
+++ src/main/java/com/escudo7/food/api/controller/RestauranteController.java
@@ -2,12 +2,14 @@ package com.escudo7.food.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +53,26 @@ public class RestauranteController {
 			return ResponseEntity.status(HttpStatus.CREATED)
 					.body(restaurante);
 		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest()
+					.body(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> atualizar(@PathVariable Long id,
+			@RequestBody Restaurante restaurante){
+		try {
+			Restaurante restauranteAtual = restauranteRepository.buscar(id);
+			
+			if (restaurante != null) {
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+				
+				restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
+				return ResponseEntity.ok(restauranteAtual);
+			}
+			
+			return ResponseEntity.notFound().build();
+		}catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest()
 					.body(e.getMessage());
 		}
