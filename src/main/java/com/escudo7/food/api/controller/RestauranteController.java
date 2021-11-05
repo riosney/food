9dc13cd8915219ec052264879509bab01fe585e2
Riosney Santos <riosney.vale@saude.ce.gov.br>
2021-11-05 10:06:35 -0300

    Criando adicionar de restaurante

--- src/main/java/com/escudo7/food/api/controller/RestauranteController.java
+++ src/main/java/com/escudo7/food/api/controller/RestauranteController.java
@@ -3,14 +3,19 @@ package com.escudo7.food.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.escudo7.food.domain.exeption.EntidadeNaoEncontradaException;
 import com.escudo7.food.domain.model.Restaurante;
 import com.escudo7.food.domain.repository.RestauranteRepository;
+import com.escudo7.food.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping(value = "/restaurantes")
@@ -18,6 +23,9 @@ public class RestauranteController {
 	
 	@Autowired
 	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CadastroRestauranteService cadastroRestaurante;
 	
 	@GetMapping
 	public List<Restaurante> listar(){
@@ -33,6 +41,19 @@ public class RestauranteController {
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante){
+		try {
+			restaurante = cadastroRestaurante.salvar(restaurante);
+			
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(restaurante);
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest()
+					.body(e.getMessage());
+		}
 	}
 
 }
