9db70f876b447dd8018a25bec1498ed59e909d0b
Riosney Santos <riosney.vale@saude.ce.gov.br>
2021-11-09 14:20:16 -0300

    Queries JPQL com @Query

--- src/main/java/com/escudo7/food/api/controller/TesteController.java
+++ src/main/java/com/escudo7/food/api/controller/TesteController.java
@@ -49,7 +49,7 @@ public class TesteController {
 	@GetMapping("/restaurantes/por-nome")
 	public List<Restaurante> restaurantePorTaxaFrete(
 			String nome, Long cozinhaId) {
-		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+		return restauranteRepository.consultarPorNome(nome, cozinhaId);
 	}
 	
 	@GetMapping("/restaurantes/primeiro-por-nome")
--- src/main/java/com/escudo7/food/domain/repository/RestauranteRepository.java
+++ src/main/java/com/escudo7/food/domain/repository/RestauranteRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.escudo7.food.domain.model.Restaurante;
@@ -14,7 +16,10 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 	
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFianl);
 	
-	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+	
+//	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
 	
 	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
 	
