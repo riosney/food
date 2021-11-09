6f884d53091be85cb181a357a95015b896775b7c
Riosney Santos <riosney.vale@saude.ce.gov.br>
2021-11-09 14:42:22 -0300

    Externalizando consultas JPQL para um arquivo XML

--- src/main/java/com/escudo7/food/domain/repository/RestauranteRepository.java
+++ src/main/java/com/escudo7/food/domain/repository/RestauranteRepository.java
@@ -16,7 +16,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 	
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFianl);
 	
-	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+//	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
 	
 //	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
