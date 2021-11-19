203ff89305163034a0d65d47553fd1447af60469
Riosney Santos <riosney.vale@saude.ce.gov.br>
2021-11-19 20:45:58 -0300

    Relacionamento @ManyToMany

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -19,3 +19,5 @@ insert into estado (id, nome) values (3, 'Ceará');
 insert into cidade (id, nome, estado_id) values (1, 'Uberlândia', 1);
 insert into cidade (id, nome, estado_id) values (2, 'Belo Horizonte', 1);
 insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
