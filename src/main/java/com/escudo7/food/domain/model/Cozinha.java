f899b993114a9d039b724d6eacfd7fb0bd208c5d
Riosney Santos <riosney.vale@saude.ce.gov.br>
2021-11-19 20:03:10 -0300

    Mapeamento bidirecional com @OneToMany

--- src/main/java/com/escudo7/food/domain/model/Cozinha.java
+++ src/main/java/com/escudo7/food/domain/model/Cozinha.java
@@ -1,11 +1,16 @@
 package com.escudo7.food.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
@@ -22,9 +27,11 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-//	@JsonIgnore
-//	@JsonProperty("titulo")
 	@Column(nullable = false)
 	private String nome;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 
 }
