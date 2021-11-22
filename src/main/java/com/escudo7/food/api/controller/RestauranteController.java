f578f5cb8ed15d46472b951c3ed7688c5b877c02
Riosney <riosney_s@hotmail.com>
2021-11-22 14:12:43 -0300

    Relacionamento um-para-muitos de restaurante

--- src/main/java/com/escudo7/food/api/controller/RestauranteController.java
+++ src/main/java/com/escudo7/food/api/controller/RestauranteController.java
@@ -73,7 +73,7 @@ public class RestauranteController {
 			
 			if (restaurante != null) {
 				BeanUtils.copyProperties(restaurante, restauranteAtual, 
-						"id", "formasPagamento", "endereco", "dataCadastro");
+						"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
 				
 				restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
 				return ResponseEntity.ok(restauranteAtual);
