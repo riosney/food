7f98626ff236434d86477ce1b9004f78c33c787d
Riosney Santos <riosney.vale@saude.ce.gov.br>
2022-01-11 20:17:14 -0300

    Criando migrações complexas

--- src/main/java/com/escudo7/food/domain/repository/RestauranteRepository.java
+++ src/main/java/com/escudo7/food/domain/repository/RestauranteRepository.java
@@ -17,7 +17,7 @@ public interface RestauranteRepository
 	extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries,
 	JpaSpecificationExecutor<Restaurante>{
 	
-	@Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+	@Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamentos")
 	List<Restaurante> findAll();
 	
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFianl);
