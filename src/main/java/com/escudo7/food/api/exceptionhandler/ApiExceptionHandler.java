189bdae932b3d4cd9bcbb9daa147957d112663c2
Riosney <rsantosdo@minsait.com>
2022-02-16 14:07:39 -0300

    Implementando exception handler

--- src/main/java/com/escudo7/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/escudo7/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.escudo7.food.domain.exeption.EntidadeEmUsoException;
 import com.escudo7.food.domain.exeption.EntidadeNaoEncontradaException;
 import com.escudo7.food.domain.exeption.NegocioException;
 
@@ -43,6 +44,16 @@ public class ApiExceptionHandler {
 				.mensagem("O tipo de mídia não é aceito").build();
 		
 		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+				.body(problema);
+	}
+	
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage()).build();
+		
+		return ResponseEntity.status(HttpStatus.CONFLICT)
 				.body(problema);
 	}
 
