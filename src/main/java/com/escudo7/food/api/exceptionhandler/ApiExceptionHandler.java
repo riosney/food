41f8ce60b61bb7d143538087b6acff83f946d04c
Riosney <rsantosdo@minsait.com>
2022-03-08 11:12:35 -0300

    Customizando exception handlers

--- src/main/java/com/escudo7/food/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/escudo7/food/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.escudo7.food.api.exceptionhandler;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -15,6 +16,19 @@ import com.escudo7.food.domain.exeption.NegocioException;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+		String detail = "O corpo da requisição está inválido. Verifique erro de sintaxe.";
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();		
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(),
+				status, request);
+	}
+	
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> handleEntidadeNaoEncontradaException(
 			EntidadeNaoEncontradaException ex, WebRequest request) {
@@ -24,13 +38,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		String detail = ex.getMessage();
 		
 		Problem problem = createProblemBuilder(status, problemType, detail).build();		
-		
-//		Problem problem = Problem.builder()
-//				.status(status.value())
-//				.type("http://food.com.br/entidade-nao-encontrada")
-//				.title("Entidade não encontrada")
-//				.detail(ex.getMessage())
-//				.build();
 		
 		return handleExceptionInternal(ex, problem, new HttpHeaders(),
 				status, request);
