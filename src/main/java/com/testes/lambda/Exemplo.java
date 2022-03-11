329d80884da221e90f966d681a76061746ac5c65
Riosney <rsantosdo@minsait.com>
2022-03-11 10:19:17 -0300

    correção da nomenclatura

--- src/main/java/com/testes/lambda/Exemplo.java
+++ src/main/java/com/testes/lambda/Exemplo.java
@@ -7,7 +7,7 @@ public class Exemplo {
 	public static void main(String[] args) {
 		List<Fatura> faturasVencidas = new FaturaDAO().buscarFaturasVencidas();
 		
-		// Referência de método
+		// Referência a métodos
 		faturasVencidas.forEach(Fatura::atualizarStatus);
 	}
 
