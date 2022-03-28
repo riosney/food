7f15178730a28b2900607f40fa4b7fb644d79086
Riosney <rsantosdo@minsait.com>
2022-03-28 10:44:29 -0300

    Adicionando as propriedades com constraints violadas

--- src/main/java/com/escudo7/food/api/exceptionhandler/Problem.java
+++ src/main/java/com/escudo7/food/api/exceptionhandler/Problem.java
@@ -1,6 +1,7 @@
 package com.escudo7.food.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -16,9 +17,18 @@ public class Problem {
 	private Integer status;
 	private String type;
 	private String title;
-	private String detail;
-	
+	private String detail;	
 	private String userMessage;
 	private LocalDateTime timestamp;
+	private List<Field> fields;
+	
+	@Getter
+	@Builder
+	public static class Field {
+		
+		private String name;
+		private String userMessage;
+		
+	}
 
 }
