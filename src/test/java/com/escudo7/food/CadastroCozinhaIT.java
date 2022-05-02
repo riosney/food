83add7b8ff44ba510accae251ef04f45d247bfe9
Riosney <rsantosdo@minsait.com>
2022-05-02 14:29:15 -0300

    Voltando o estado inicial do banco com callback do Flyway

--- src/test/java/com/escudo7/food/CadastroCozinhaIT.java
+++ src/test/java/com/escudo7/food/CadastroCozinhaIT.java
@@ -1,9 +1,11 @@
 package com.escudo7.food;
 
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -19,11 +21,16 @@ public class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 	
+	@Autowired
+	private Flyway flyway;
+	
 	@Before
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+		
+		flyway.migrate();
 	}
 
 	@Test
