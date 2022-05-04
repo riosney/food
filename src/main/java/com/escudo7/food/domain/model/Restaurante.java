3c89fd9c6f76b6c5ba5a60e25bb61b6790e8d01d
Riosney <rsantosdo@minsait.com>
2022-05-04 15:17:13 -0300

    Refinando o payload de cadastro com @JsonIgnoreProperties

--- src/main/java/com/escudo7/food/domain/model/Restaurante.java
+++ src/main/java/com/escudo7/food/domain/model/Restaurante.java
@@ -28,6 +28,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import com.escudo7.food.core.validation.Groups;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -51,6 +52,7 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
+	@JsonIgnoreProperties(value = "nome", allowGetters = true)
 	@Valid
 	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
 	@NotNull
