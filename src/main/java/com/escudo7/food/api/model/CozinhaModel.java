059ae10457322d62704595ad731c96930cdd869d
Riosney <rsantosdo@minsait.com>
2022-06-07 14:39:06 -0300

    Isolando o Domain Model do Representation Model com o padrão DTO

---
+++ src/main/java/com/escudo7/food/api/model/CozinhaModel.java
@@ -0,0 +1,13 @@
+package com.escudo7.food.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CozinhaModel {
+	
+	private Long id;
+	private String nome;
+
+}
